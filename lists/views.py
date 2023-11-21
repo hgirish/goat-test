c1fd3a8bd81dde51cab9b6bfd802cc7ae8a745bd
Girish <girish@post.com>
2023-11-20 19:03:53 -0800

    Redirect after post

--- lists/views.py
+++ lists/views.py
@@ -6,9 +6,8 @@ from lists.models import Item
 
 def home_page(request: HttpRequest):
     if request.method == "POST":
-        item = Item()
-        item.text = request.POST.get("item_text", "")
-        item.save()
+
+        Item.objects.create(text=request.POST["item_text"])
         return redirect("/")
 
     return render(
