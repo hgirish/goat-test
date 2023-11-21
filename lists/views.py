c8cb13514a74170c73d57b71f946df0ca00ccf98
Girish <girish@post.com>
2023-11-20 18:32:52 -0800

    time to refactor

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def home_page(request):
-    return render(request, "home.html")
+def home_page(request: HttpRequest):
+    return render(
+        request,
+        "home.html",
+        {"new_item_text": request.POST.get("item_text", "")})
